e86fcaf73237a1b5a4512fe865105e9b8310cd75
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-16 18:33:05 -0700

    up to lec 110

---
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5.0f;
+	public float speed = 5.0f;
+	public float padding;
+
+	private float xmin;
+	private float xmax;
+	private bool movingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+		// Decides the edges
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera));
+		xmin = leftBoundary.x + padding;
+		xmax = rightBoundary.x - padding;
+
+		// Clones enemy for each position object under EnemyFormation
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity); // as GameObject?
+			enemy.transform.parent = child; 
+		}
+
+
+	}
+
+	public void OnDrawGizmos () {
+		Gizmos.DrawWireCube (transform.position, new Vector3 (width, height));
+	}
+	// Update is called once per frame
+	void Update () {
+
+		// My try
+		/*transform.position += Vector3.right * speed * Time.deltaTime;
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		if (newX == xmin || newX == xmax) {
+			speed *= -1;
+		}*/
+
+		// By Lecture
+		if (movingRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else {
+			transform.position -= Vector3.right * speed * Time.deltaTime;
+		}
+
+		float rightEdgeOfFormation = transform.position.x + 0.5f * width;
+		float leftEdgeOfFormation = transform.position.x - 0.5f * width;
+		if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
+			movingRight = !movingRight;
+		}// Can take the width of the Gizmo's width into account.
+
+
+
+	}
+}
--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 5.0f;
-	public Rigidbody2D player;
+	public float speed;
+	public float padding;
+	//public Rigidbody2D player; // thought I need this...
+
+	float xmin;
+	float xmax;
 
 	// Use this for initialization
 	void Start () {
-		
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+			//transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime; // more readable and simpler than the way above
+
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+			//transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime; // more readable and simpler than the way above
+
 		} else if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+			//transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+			transform.position += Vector3.up * speed * Time.deltaTime; // more readable and simpler than the way above
+
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.position += new Vector3 (0, -speed * Time.deltaTime, 0);
+			//transform.position += new Vector3 (0, -speed * Time.deltaTime, 0);
+			transform.position += Vector3.down * speed * Time.deltaTime; // more readable and simpler than the way above
+
 		} 
+
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 }
