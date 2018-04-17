a0d7ba22a0947af718fb9e3370ed169b5d066fce
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-17 11:16:55 -0700

    up to second mid quiz

---
+++ Laser Defender/Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public GameObject projectile;
+	public float health;
+	public float firingRate;
+	public float ShotsPerSeconds;
+
+	void Update () {
+		float probability = Time.deltaTime * ShotsPerSeconds;
+		if (Random.value < probability) {
+			Fire ();
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		//Debug.Log (col);
+		Projectile missile = col.gameObject.GetComponent<Projectile> ();
+		if (missile) {
+			health -= missile.GetDamage ();
+			missile.Hit ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+		
+	void Fire () {
+		Vector3 startPosition = transform.position + new Vector3 (0, -1, 0);
+		GameObject beam = Instantiate (projectile, startPosition, Quaternion.identity);
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, -projectile.GetComponent<Projectile>().speed, 0);
+	}
+
+}
---
+++ Laser Defender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage;
+	public float speed;
+
+	public float GetDamage () {
+		return damage;
+	}
+
+	public void Hit () {
+		Destroy (gameObject); // self-destruction
+	}
+
+	public float GetSpeed () {
+		return speed;
+	}
+}
